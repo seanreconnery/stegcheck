0e719df46b1a5d323d641f768752a8b97d8fcd44
seanreconnery <56751730+seanreconnery@users.noreply.github.com>
2019-12-04 14:13:14 -0500

    added setup for StegDetect, Outguess & Strings

---
+++ steg-check-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# Download STEGDETECT
+wget -O /tmp/stegdetect.deb http://old-releases.ubuntu.com/ubuntu/pool/universe/s/stegdetect/stegdetect_0.6-6_amd64.deb
+
+# Install STEGDETECT
+dpkg -i /tmp/stegdetect.deb || apt-get install -f -y
+rm /tmp/stegdetect.deb
+
+# Download OUTGUESS-0.13
+wget -O /usr/bin/outguess-0.13 https://github.com/mmayfield1/SSAK/raw/master/programs/64/outguess_0.13
+chmod +x /usr/bin/outguess-0.13
+
+# Download OUTGUESS-0.2
+wget -O /usr/bin/outguess https://github.com/mmayfield1/SSAK/raw/master/programs/64/outguess_0.2
+chmod +x /usr/bin/outguess
+
+# Download STRINGS
+wget -O /usr/bin/strings https://github.com/mmayfield1/SSAK/raw/master/programs/64/strings
+chmod +x /usr/bin/strings
+
